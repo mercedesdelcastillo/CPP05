6b2c2512da3e1f9bbfaa3baceea4c9cc14c575af
mercedesdelcastillo <meru334@gmail.com>
2026-07-15 18:07:01 +0200

    bonsai

--- ex02/src/ShrubberyCreationForm.cpp
+++ ex02/src/ShrubberyCreationForm.cpp
@@ -1,5 +1,24 @@
 #include "ShrubberyCreationForm.hpp"
 
+std::string tree0 = "#                a&                                  \n";
+std::string tree1 = "#              8&@&                                  \n";
+std::string tree2 = "#       &8@8 8&  8@|                                 \n";
+std::string tree3 = "#    &&@@&%#@%_|@@&&@#       @=&                     \n";
+std::string tree4 = "#   &#@# #%##  ;&@8#  %@    @% &%                    \n";
+std::string tree5 = "#     @  %  #&  8%~|       @%@%@&#                  \n";
+std::string tree6 = "#                  |;;         # %                   \n";
+std::string tree7 = "#                     \\        @ 8 %@%#             \n";
+std::string tree8 = "#                      |~     =;@ __8%               \n";
+std::string tree9 = "#                      =|   ~_=___  %&#              \n";
+std::string tree10  = "#                      || /~         % #             \n";
+std::string tree11 = "#                      |//           &               \n";
+std::string tree12 ="#                      |=                            \n";
+std::string tree13 = "#                      ~|                            \n";
+std::string tree14 = "#                      ;|                            \n";
+std::string tree15 = "#       .---.        ./||\\.    .-.";
+
+std::string tree[] = {tree0, tree1, tree2, tree3, tree4, tree5, tree6, tree7, tree8, tree9, tree10, tree11, tree12, tree13, tree14, tree15};
+
 //Constructors
 ShrubberyCreationForm::ShrubberyCreationForm(void) : AForm()
 {
@@ -44,7 +63,10 @@ void    ShrubberyCreationForm::create_file(std::string name)
 		std::cout << "Error opening " << name << "\n";
 		return;
 	}
-    
+    for(int i = 0; i < 16; i++)
+    {
+        wFile << tree[i];
+    }
     wFile.close();
 }
 
